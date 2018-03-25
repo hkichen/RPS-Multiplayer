e694b376bb74a4e0406a428bd092435a4539d5f8
hkichen <hkichen@gmail.com>
2018-03-25 17:47:58 -0400

    basic skeleton

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+//on start button, start new game
+//player 1 chooses, then player two chooses
+//compare choices and determine win
+//display who wins round
+//update score
+//after scoring, a new game begins unless user presses stop 
+
+//store the player's interactions in firebase
+//object stored in firebase should log player 1's name, choice, wins, losses
+//at the same time, it should store player 2's name, choice, wins, and losses
